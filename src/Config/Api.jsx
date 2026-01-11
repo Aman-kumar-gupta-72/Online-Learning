1f15bdcd26de3c1c2b0f73230cbc7b74047e51b7
Aman kumar <ag86898138@gmail.com>
2026-01-11 14:50:29 +0530

    change it

--- src/Config/Api.jsx
+++ src/Config/Api.jsx
@@ -1,3 +1,8 @@
-const API = import.meta.env.VITE_API_URL;
+const API =
+  typeof import.meta !== "undefined" &&
+  import.meta.env &&
+  import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL
+    : "";
 
 export default API;
